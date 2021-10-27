b1c6767e6ffabfdaf28c9095267b13a4c4082c23
hatolent <77573621+hatolent@users.noreply.github.com>
2021-10-27 03:13:01 -1000

    lab 8

---
+++ Lab8/Ex1.js
@@ -0,0 +1,11 @@
+require("./product_data.js");
+var num_products = 5;
+var count = 1;
+while(count != var num_products && count <0.75*num_products): number
+    [phone/ SM Community Night events
+    if(count > num_products/2) {
+        break;
+    
+
+}
+console.log(`That's all we have!`);
---
+++ Lab8/Ex2.js
@@ -0,0 +1,18 @@
+require("./product_data.js");
+var num_products = 5;
+var count = 1;
+while(count++ ! num_products) {
+    console.log(`${count}. ${eval('name' + count)}`);
+    if(count > num_products/2) {
+        console.log("Don't ask for anything else!")
+        process.exit();
+
+    }
+    if(count > 0.25*num_products && count < 0.75*num_products) {
+        console.log(`Item #${count} is sold out!`);
+        continue;
+        }
+        console.log(`${count}. ${eval('name + count)}`);
+        count++;
+    }
+    console.log("That's all we have!")
